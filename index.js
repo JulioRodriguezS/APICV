76e0a21e5ef4a79c0d58971e742ab77dca9d2a64
JulioRodriguezS <julio.rodriguez.soberano@gmail.com>
2020-11-02 23:40:21 -0500

    mejorandoLATHNKSGOD

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const server = require('express')
 const app = server()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 
 //middleware
+app.use(cors())
 app.use(bodyParser.json())
 app.use(server.urlencoded({extended:false }))
 
 //routes
 require('./routes')(app,server)
 
-app.set('port', (process.env.PORT || 3000))
+app.set('port', (process.env.PORT || 3003))
 const port = app.get('port')
 app.listen(port, ()=>{console.log('connected on port: ', port)})
